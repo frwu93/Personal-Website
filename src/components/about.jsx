f91731718f719c5d746a5e73fe506af59d0cb7a9
frwu93 <franklinw888@gmail.com>
2021-01-30 22:57:26 -0500

    started introduction and about sections

---
+++ src/components/about.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+export default class About extends Component {
+    render(){
+        return(
+            <div>
+                <h2 className="colorlib-heading">About Me</h2>
+                <section>
+                    <p>
+                        <strong>This is the about me section.</strong>
+                    </p>
+                </section>
+            
+            </div>
+        )
+    }
+}
---
+++ src/components/introduction.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+export default class Introduction extends Component {
+    render(){
+        return(
+            <div>
+                <h2 className="colorlib-heading">Introduction</h2>
+                <section>
+                    <p>
+                        <strong>This is the introduction.</strong>
+                    </p>
+                </section>
+                
+            </div>
+        )
+    }
+}
